3124dd23f913a8238bacc03811f1078bd9a0889e
mklee-mac <mklee@headit.co.kr>
2023-09-19 17:47:03 +0900

    update - QnaRoomQnaController uri

--- src/interface/qna-room-qna/qna-room-qna.controller.ts
+++ src/interface/qna-room-qna/qna-room-qna.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 
-@Controller()
+@Controller('api/qna-rooms/:roomId/questions')
 @ApiTags('QnaRoomQnaController')
 export class QnaRoomQnaController {
   
