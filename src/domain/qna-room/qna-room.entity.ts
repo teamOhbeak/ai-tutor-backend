dca73138940dda1f6e56e878a64d41a0483f5d17
Yuseon Park <pyuseon@gmail.com>
2023-09-20 00:41:57 +0900

    feat: add QnaRoom entity

---
+++ src/domain/qna-room/qna-room.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class QnaRoom {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  title: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  deleted: boolean
+
+
+
+}
