22ff35df1aa34e81545ad23093a1cf753af09a87
mklee-mac <mklee@headit.co.kr>
2023-09-19 17:42:03 +0900

    add - QnaRoomQnaController

--- src/domain/qna-room/qna-room.module.ts
+++ src/domain/qna-room/qna-room.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { QnaRoomQnaController } from 'src/interface/qna-room-qna/qna-room-qna.controller';
 import { QnaRoomController } from 'src/interface/qna-room/qna-room.controller';
 @Module({
   imports: [],
   controllers: [
-    QnaRoomController
+    QnaRoomController,
+    QnaRoomQnaController,
   ],
   providers: [],
 })
