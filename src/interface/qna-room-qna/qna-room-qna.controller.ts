9126914a3b914f05244c228a721dcc1b7b73f7a6
mklee-mac <mklee@headit.co.kr>
2023-09-19 18:01:23 +0900

    add QnaRoomQnaController - createQuestion, getQuestions

--- src/interface/qna-room-qna/qna-room-qna.controller.ts
+++ src/interface/qna-room-qna/qna-room-qna.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller('api/qna-rooms/:roomId/questions')
 @ApiTags('QnaRoomQnaController')
 export class QnaRoomQnaController {
   
   constructor() { }
+
+  @Post()
+  @ApiCreatedResponse()
+  async createQuestion(
+    @Param('roomId') roomId: number, 
+    @Body() params: any)
+  : Promise<any> {
+
+    return {id: 'questionId', contents: ''};
+  }
+
+  @Get()
+  @ApiOkResponse()
+  async getQuestions(
+    @Param('roomId') roomId: number)
+  : Promise<any> {
+    
+    return [];
+  }
 }
