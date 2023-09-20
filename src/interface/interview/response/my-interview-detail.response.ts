43f57d22f0fb39b075f9e693f2ad1b0fb86ba0cb
STEVE <ymyd.steve@gmail.com>
2023-09-21 07:58:53 +0900

    fix : adding types

--- src/interface/interview/response/my-interview-detail.response.ts
+++ src/interface/interview/response/my-interview-detail.response.ts
@@ -1,16 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { QuestionResponse } from './question.response';
 
+export enum InterviewStatus {
+  COMPLETED = 0,
+  IN_PROGRESS = 1,
+  WAITING = 2,
+  CANCELED = 3,
+  EXPIRED = 4,
+  NO_SHOW = 5,
+}
+
+export enum StacktType {
+  JAVA = 0,
+  JAVASCRIPT = 1,
+  KOTLIN = 2,
+  REACTJS = 3,
+  NEXTJS = 4,
+  NODEJS = 5,
+  NESTJS = 6,
+  SPRING = 7,
+  CS = 8,
+}
 export class MyInterviewDetailResponse {
   @ApiProperty({ example: 1000 })
   id: number;
 
-  @ApiProperty({ example: 'U' })
-  status: string;
+  @ApiProperty({ example: 'U', enum: InterviewStatus })
+  status: InterviewStatus;
 
   // TODO: enum 선언되면 교체
   @ApiProperty({ example: 'java' })
-  stack: string;
+  stack: StacktType;
 
   @ApiProperty()
   questionCount: number;
@@ -29,7 +49,7 @@ export class MyInterviewDetailResponse {
 
   constructor(
     id: number,
-    status: string,
+    status: InterviewStatus,
     createdAt: string,
     questions: QuestionResponse[],
   ) {
