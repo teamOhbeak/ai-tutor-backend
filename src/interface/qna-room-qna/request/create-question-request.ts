a8de94f863339cac7a361eb08f70b62518d01e4e
Yuseon Park <pyuseon@gmail.com>
2023-09-22 00:15:41 +0900

    modify request

--- src/interface/qna-room-qna/request/create-question-request.ts
+++ src/interface/qna-room-qna/request/create-question-request.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsString, isInt } from 'class-validator';
 
 export class CreateQuestionRequest {
   @ApiProperty({ example: '1000' })
+  @IsInt()
   roomId: number;
 
   @ApiProperty({ example: 'this is qusetion.' })
+  @IsString()
   question: string;
 
   constructor(roomId: number, question: string) {
