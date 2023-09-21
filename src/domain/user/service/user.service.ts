ab5ac6382bb02d91162ffa3b931e00ba906ca3e2
mklee-mac <mklee@headit.co.kr>
2023-09-21 18:18:02 +0900

    add userService

---
+++ src/domain/user/service/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { UserRepository } from "../repository/user.repository";
+import { UserEntity } from "../entity/user.entity";
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {}
+
+  async findUserById(userId: number)
+  : Promise<UserEntity> {
+    return this.userRepository.findUserById(userId);
+  }
+}
