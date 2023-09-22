79f73b3954fddf00b0bb4f101746afef03d7f8f8
mklee-mac <mklee@headit.co.kr>
2023-09-23 00:16:05 +0900

    backup

--- src/domain/interview/service/interview.facade.ts
+++ src/domain/interview/service/interview.facade.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InterviewService } from './interview.service';
 import { InterviewQuestionService } from '@/domain/interview-question/service/interview-question.service';
 import { CreateInterviewRequest } from '@/interface/interview/request/create-interview.request';
-import { InterviewEntity } from '../entity/interview.entity';
 import { IFakeQuestionBankService } from '@/domain/questionsBank/service/fake-questionBank.interface';
 import { FakeQuestionBankService } from '@/domain/questionsBank/service/fake-questionBank.service';
 import { InterviewQuestionUtil } from '../utils/interview-question.util';
@@ -22,25 +21,22 @@ export class InterviewFacade {
     userId: number,
     dto: CreateInterviewRequest,
   ): Promise<CreateInterviewResponse> {
-    const interviewRoom = await this.interviewService.createInterview(
-      userId,
-      dto,
-    );
+    const interviewRoom = await this.interviewService
+      .createInterview(userId, dto);
 
-    const questions = await this.questionBankService.getFakeQuestions(
-      dto.questionCount,
-    );
-
-    const interviewQuestions = InterviewQuestionUtil.generateInterviewQuestion(
-      interviewRoom.id,
-      questions,
-    );
-
-    interviewRoom.questions =
-      await this.interviewQuestionService.saveInterviewQuestions(
-        interviewQuestions,
+    const questions = await this.questionBankService
+      .getFakeQuestions(dto.questionCount);
+    
+    const interviewQuestions = InterviewQuestionUtil
+      .generateInterviewQuestion(
+        interviewRoom.id,
+        questions,
       );
+    
+    interviewRoom.questions = await this.interviewQuestionService
+      .saveInterviewQuestions(interviewQuestions);
 
-    return InterviewQuestionUtil.toCreateInterviewResponse(interviewRoom);
+    return InterviewQuestionUtil
+      .toCreateInterviewResponse(interviewRoom);
   }
 }
