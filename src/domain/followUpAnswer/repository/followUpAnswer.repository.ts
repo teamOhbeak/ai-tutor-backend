2e1c16cd6e8e936f44e632053f810ff9337004cd
sejin park <seijin0722@naver.com>
2023-09-23 02:36:01 +0900

    followUpAnswer 엔티티,리포지토리 생성

--- src/domain/followUpAnswer/repository/followUpAnswer.repository.ts
+++ src/domain/followUpAnswer/repository/followUpAnswer.repository.ts
@@ -1,14 +1,14 @@
 import { DataSource, EntityRepository, Repository } from 'typeorm';
 import { FollowUpAnswer } from '../entity/followUpAnswer.entity';
-import { FollowUpAnswerRepository } from './followUpAnswer.repository.interface';
 import { Inject } from '@nestjs/common';
 
 @EntityRepository(FollowUpAnswer)
-export class FollowUpAnswerRepositoryImpl
-  extends Repository<FollowUpAnswer>
-  implements FollowUpAnswerRepository
-{
+export class FollowUpAnswerRepositoryImpl extends Repository<FollowUpAnswer> {
   constructor(@Inject('DATA_SOURCE') private readonly dataSource: DataSource) {
     super(FollowUpAnswer, dataSource.createEntityManager());
+  }
+  someProperty: string;
+  someMethod(): void {
+    throw new Error('Method not implemented.');
   }
 }
