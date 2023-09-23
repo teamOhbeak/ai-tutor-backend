e3c5c2b5f84f590ff597725b28fecd2192f00f45
Yuseon Park <pyuseon@gmail.com>
2023-09-23 15:51:05 +0900

    fix :  modify prompt not to act like ai

--- src/domain/prompt/service/prompt.service.ts
+++ src/domain/prompt/service/prompt.service.ts
@@ -155,20 +155,6 @@ export class PromptService {
 
     const messages = [];
 
-    // if (qna) {
-    //   messages.push({
-    //     role: 'user',
-    //     content: qna.question,
-    //   });
-    //   messages.push({
-    //     role: 'system',
-    //     content:
-    //       qna.answer !== '답변이 없습니다.'
-    //         ? qna.answer
-    //         : '이전 답변이 없습니다.',
-    //   });
-    // }
-
     if (qnaList && qnaList.length > 0) {
       // Iterate through qnaList and push each Qna's question and answer as messages
       qnaList.forEach((qna) => {
@@ -191,13 +177,13 @@ export class PromptService {
           '당신은 질문의 답변을 하는 IT 에시스턴스 입니다.' +
           '\n 질문의 답변을 해주세요. 질문을 하는 사람은 배우는 입장이기 때문에. ' +
           '\n 질문의 의도가 이해되지 않는데면 질문을 파악하기 위한 다른 질문을 해도 됩니다.' +
-          '\n 다만 절대로 질문자의 질문을 똑같이 반복해서 물어보지 마세요.',
+          '\n 다만 절대로 질문자의 질문을 똑같이 반복해서 물어보지 마세요. 그리고 질문자의 질문을 그대로 다시 질문자에게 보여주지 마세요.',
       },
       {
         role: 'user',
         content:
           question +
-          '\n 한국어로 답변해줘. 그리고 내 질문을 그대로 다시 나한테 보여주진 마.',
+          '\n 한국어로 답변해줘. 그리고 내 질문을 그대로 다시 나한테 보여주진 마. \n 그리고 답변을 할때 부드러운 어조로 그리고 체를 사용해줘.',
       },
     );
 
